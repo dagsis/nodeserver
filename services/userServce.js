7a3443a2071a4d4f57c582b97ea9ad4c874f4a63
Carlos DAgostino <dagsis@dagsis.com.ar>
2018-09-15 07:24:13 -0300

    User 4

--- services/userServce.js
+++ services/userServce.js
@@ -23,7 +23,7 @@ var config = {
           });     
         } */
             
-module.exports.existeUsuario = function(nick,email,done) {
+/* module.exports.existeUsuario = function(nick,email,done) {
     GetUserExit(nick, email, function(recordset) { 
         var resul = false;               
         if (recordset.rowsAffected !=0)
@@ -32,6 +32,28 @@ module.exports.existeUsuario = function(nick,email,done) {
         }
         return done(null,resul);               
     });      
+} */
+
+module.exports.existeUsuario = function(nick,email,done) {
+    var connection = new sql.connect(config, function(err) {
+
+        // check for error by ...
+        var request = new sql.Request(connection);
+
+        request.input('nick', sql.VarChar, nick);
+        request.input('email', sql.VarChar, email);
+        request.query('select * from users WHERE email=@email or nick=@nick', 
+                        function(err, recordset) {
+                                
+                                sql.close();           
+                                var resul = false;               
+                                if (recordset.rowsAffected !=0)
+                                {
+                                    resul = true;                   
+                                }
+                                return done(null,resul);       
+                        });
+    });
 }
 
 function GetUserExit(nick,email, callback) {
@@ -43,8 +65,9 @@ function GetUserExit(nick,email, callback) {
 
         request.input('nick', sql.VarChar, nick);
         request.input('email', sql.VarChar, email);
-        request.query('select * from users WHERE email=@email and nick=@nick', 
+        request.query('select * from users WHERE email=@email or nick=@nick', 
                         function(err, recordset) {
+                                
                                 sql.close();           
                                 callback(recordset);           
                         });
