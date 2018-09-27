991d105d55fe8694f4115def18a6757295f93d40
Carlos DAgostino <dagsis@dagsis.com.ar>
2018-09-27 07:37:23 -0300

    follow 1

--- routes/follow.js
+++ routes/follow.js
@@ -7,8 +7,8 @@ var md_auth = require('../middlewares/autenticated');
 
 var api = express.Router();
 
-api.post('/followAdd', md_auth.ensureAuth, FollowController.saveFollow);
-api.delete('/followDel/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 api.get('/getMyFollows/:id?', md_auth.ensureAuth, FollowController.getMyFollows);
 api.get('/getYourFollows/:id?', md_auth.ensureAuth, FollowController.getFollowBacks);
 
